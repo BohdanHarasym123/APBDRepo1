7b86e2224df9f9ed4d321ef1416d6b63b867d890
Bohdan Harasym <s31539@pjwstk.edu.pl>
2025-03-03 16:52:12 +0100

    Added some nonsensical code

--- APBDtut1/APBDtut1/Program.cs
+++ APBDtut1/APBDtut1/Program.cs
@@ -4,3 +4,11 @@ Console.WriteLine("Commit 1");
 Console.WriteLine("Just for the fun of it");
 Console.WriteLine("And another one");
 Console.WriteLine("Adding only 1 file");
+
+static double CalcAvg(int[] arr)
+{
+    return 0;
+}
+
+int[] arr = {1, 2, 3, 5, 6};
+Console.WriteLine("Calculated average... :" + CalcAvg(arr));
