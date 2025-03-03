3209fdcb77f0da93c0d061161c640d35cb14b96e
Bohdan Harasym <s31539@pjwstk.edu.pl>
2025-03-03 16:46:37 +0100

    Commiting only 1 file

--- APBDtut1/APBDtut1/Program.cs
+++ APBDtut1/APBDtut1/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Commit 1");
 Console.WriteLine("Just for the fun of it");
 Console.WriteLine("And another one");
+Console.WriteLine("Adding only 1 file");
